ec4e632f1f8fdfb274d1f581d966d8aad0463080
takata.natsuki <takata.natsuki@host.docker.internal>
2023-06-22 16:59:29 +0900

    new

--- src/Lesson1/Lesson1.java
+++ src/Lesson1/Lesson1.java
@@ -1,9 +1,21 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class Lesson1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("数値を入力してください。");
+		Scanner scan = new Scanner(System.in);
+		String word = scan.nextLine();
+	
+		try {
+			int num = Integer.parseInt(word);
+			System.out.println(num);
+			
+		}catch(NumberFormatException ex){
+			System.out.println("数字ではありません。");
+		}
 
 	}
 
